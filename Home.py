edb3e453b1be9d1e451a9a324c83dde58df64783
blazeAssault26 <anmoltuli26@gmail.com>
2024-06-12 04:22:41 +0530

    added the pages directory

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome",
+    page_icon="👋",
+)
+
+
+st.write("# Welcome to Transaction Harbor")
+
+st.markdown(
+    """
+    These Two Apps Assist in Reviewing Customer Transactions 
+
+    **👈 Select an app from the sidebar** to get started
+
+"""
+)
